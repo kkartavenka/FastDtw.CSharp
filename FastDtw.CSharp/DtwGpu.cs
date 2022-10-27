542d7788cd3860fd41a3bc7c864da7d182a00356
Kanstantsin Kartavenka <kostya.kartavenka@syft.com>
2022-10-28 01:12:16 +0200

    Kernel is too slow

--- FastDtw.CSharp/DtwGpu.cs
+++ FastDtw.CSharp/DtwGpu.cs
@@ -8,58 +8,52 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FastDtw.CSharp; 
-internal class DtwGpu {
-    static void Kernel(Index1D i, Index1D j, ArrayView<double> arrayA, ArrayView<double> arrayB) {
+public static class DtwGpu {
 
-    }
+    public static double GetScore(double[] arrayA, double[] arrayB) {
+        var (aLength, bLength) = (arrayA.Length + 1, arrayB.Length + 1);
 
-    public double GetScore(double[] arrayA, double[] arrayB) {
-        using var context = Context.Create(_ => _.EnableAlgorithms());
-        using var accelerator = context.GetPreferredDevice(preferCPU: false).CreateAccelerator(context);
+        using var context = Context.CreateDefault();
+        using var accelerator = context.GetCudaDevice(0).CreateCudaAccelerator(context);
 
         var allocArrayA = accelerator.Allocate1D(arrayA);
         var allocArrayB = accelerator.Allocate1D(arrayB);
-        var allocDtw = accelerator.Allocate2D()
+        var allocDtwBuffer = accelerator.Allocate2DDenseY<double>(new LongIndex2D(aLength, bLength));
 
-        Action<Index1D, Index1D, ArrayView<double>, ArrayView<double>> loadedKernel =
-           accelerator.LoadAutoGroupedStreamKernel<Index1D, Index1D, ArrayView<double>, ArrayView<double>>(Kernel);
+        var loadedKernel = accelerator.LoadAutoGroupedStreamKernel<
+            Index2D,
+            ArrayView1D<double, Stride1D.Dense>,
+            ArrayView1D<double, Stride1D.Dense>,
+            ArrayView2D<double, Stride2D.DenseY>>(ExecuteKernel);
 
+        loadedKernel(allocDtwBuffer.Extent.ToIntIndex(), allocArrayA, allocArrayB, allocDtwBuffer);
 
+        return 0;
+    }
 
-        var (aLength, bLength) = (arrayA.Length + 1, arrayB.Length + 1);
-
-        var spanA = arrayA.AsSpan();
-        var spanB = arrayB.AsSpan();
-
-        double[][] dtw = new double[aLength][];
-        dtw[0] = new double[bLength];
-
-        double[] currentDtwRow;
-        var previousDtwRow = dtw[0];
+    private static void ExecuteKernel(Index2D arg1, ArrayView1D<double, Stride1D.Dense> arg2, ArrayView1D<double, Stride1D.Dense> arg3, ArrayView2D<double, Stride2D.DenseY> arg4) {
+        var aLength = arg2.Length;
+        var bLength = arg3.Length;
 
         for (var i = 1; i < aLength; i++) {
-            currentDtwRow = new double[bLength];
-
             for (var j = 1; j < bLength; j++) {
-                var cost = Math.Abs(spanA[i - 1] - spanB[j - 1]);
+                var cost = Math.Abs(arg2[i - 1] - arg3[j - 1]);
 
                 double lastMin;
                 if (i == 1 && j == 1)
-                    lastMin = previousDtwRow[j - 1];
+                    lastMin = arg4[i - 1, j - 1];
                 else if (i == 1)
-                    lastMin = currentDtwRow[j - 1];
+                    lastMin = arg4[i, j - 1];
                 else if (j == 1)
-                    lastMin = previousDtwRow[j];
+                    lastMin = arg4[i - 1, j];
                 else
-                    lastMin = Math.Min(previousDtwRow[j], Math.Min(currentDtwRow[j - 1], previousDtwRow[j - 1]));
+                    lastMin = Math.Min(arg4[i - 1, j], Math.Min(arg4[i, j - 1], arg4[i - 1, j - 1]));
 
-                currentDtwRow[j] = cost + lastMin;
+                arg4[i, j] = cost + lastMin;
             }
-            dtw[i] = currentDtwRow;
-            previousDtwRow = currentDtwRow;
         }
 
-        return dtw[aLength - 1][bLength - 1];
+        //Interop.WriteLine($"A: {aLength}, B: {bLength}");
+        Interop.WriteLine("{0}", arg4[aLength - 1, bLength - 1]);
     }
-
 }
---
+++ Sample/Program.cs
@@ -0,0 +1,54 @@
+﻿using FastDtw.CSharp;
+using System;
+using System.IO;
+
+namespace Sample; 
+internal class Program {
+
+    private const string _testFile = @".\..\..\..\..\Data\Test.csv";
+
+    static void Main() {
+        var data = GetData();
+
+        Console.WriteLine($"DTW score (double): {Dtw.GetScore(data.arrayA, data.arrayB)}");
+        Console.WriteLine($"DTW score (float): {Dtw.GetScoreF(data.arrayAF, data.arrayBF)}");
+        Console.WriteLine($"DTW score (double) [GPU]: {DtwGpu.GetScore(data.arrayA, data.arrayB)}");
+    }
+
+    private static (double[] arrayA, double[] arrayB, float[] arrayAF, float[] arrayBF) GetData() {
+        var lines = File.ReadAllLines(_testFile);
+
+        var arrayA = new double[lines.Length];
+        var arrayB = new double[lines.Length];
+        var arrayAF = new float[lines.Length];
+        var arrayBF = new float[lines.Length];
+
+        int idxA = 0, idxB = 0;
+        foreach (var line in lines) {
+            var splittedString = line.Split(',');
+
+            if (double.TryParse(splittedString[0], out double varA)) {
+                arrayAF[idxA] = (float)varA;
+                arrayA[idxA++] = varA;
+            }
+
+            if (double.TryParse(splittedString[1], out double varB)) {
+                arrayBF[idxB] = (float)varB;
+                arrayB[idxB++] = varB;
+            }
+        }
+
+        if (idxA != arrayA.Length) {
+            arrayA = arrayA[0..idxA];
+            arrayAF = arrayAF[0..idxA];
+        }
+
+        if (idxB != arrayB.Length) {
+            arrayB = arrayB[0..idxB];
+            arrayBF = arrayBF[0..idxB];
+        }
+
+        return (arrayA[0..idxA], arrayB[0..idxB], arrayAF[0..idxA], arrayBF[0..idxB]);
+    }
+
+}
